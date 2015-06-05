fd2f7f9090f187bf6a51b925ab876c6510f509be
Peter Aldaron <peteraldaron@users.noreply.github.com>
2015-06-04 23:24:58 -0400

    Update word.py

--- word.py
+++ word.py
@@ -5,3 +5,11 @@ def hamming_distance(s1, s2):
     if len(s1) != len(s2):
         raise ValueError("Undefined for sequences of unequal length")
     return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
+
+class Word:
+    def __init__(self, title, wordType):
+        self.title = title;
+        self.wordType = wordType;
+    def __str__(self):
+        return self.title + " " + self.wordType;
