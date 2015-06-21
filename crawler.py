87299928077edfd3f012b320a6f1e59fddc45c83
Peter Zhang <peteraldaron@gmail.com>
2015-06-21 14:59:34 -0400

    wip crawler2

--- crawler.py
+++ crawler.py
@@ -39,7 +39,33 @@ def crawlSpace(initWord="olla", language="Finnish", collectionName="finnish"):
                         queue.extend(q.linksIterator)
 
 
-def crawlSpace2(initWord="olla", language="Finnish", collectionName="finnish"):
-    visitedSet = set()
+def queryAndReturnNeighbors(page, collection, visitedSet, language):
+    if not isinstance(page, pw.Page):
+        return set()
+    #return empty set:
     q = query.Query()
+    title = page._link.canonical_title()
+    if title in visitedSet:
+        return set()
+    else:
+        print(title)
+        if ("Category" in title and language in title):
+            #add links:
+            print("added category")
+            return set(page.linkedPages())
+        elif ("Appendix" in title):
+            return set()
+        else:
+            wordString = q.parseContentOfLanguage(title, q.getContentOfPage(page), language)
+            if len(wordString):
+                wordObj =  word.Word(title, language, wordString);
+                dataBase.upsertOneToCollection(wordObj.toObject(), wordObj.hashValueDict, collectionName)
+                return set(q.linksIterator)
+            else:
+                return set()
+
+
+def crawlSpace2(initWord="olla", language="Finnish", collectionName="finnish"):
     dataBase = db.DataBase()
+    visitedSet = set(dataBase.getAllVisitedTitles(collectionName))
+
